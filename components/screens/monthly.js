9e491e7b35a7a1eb86381cb31e1ee4dff3e5050b
brianngeywo <brianngeywo254@gmail.com>
2020-11-24 12:00:11 +0300

    mopesa app

--- components/screens/monthly.js
+++ components/screens/monthly.js
@@ -1,69 +1,16 @@
-import React from 'react'
-import { Button, Text, StyleSheet, View, SafeAreaView, ScrollView } from "react-native";
+import React, { Component } from "react";
+import { View, Text } from "react-native";
 
-
-export default function Monthly() {
+class Monthly extends Component {
+  render() {
     return (
-        <SafeAreaView>
-            <ScrollView style={styles.container}>
-                <Text style={{marginHorizontal: 20,marginVertical: 10,textTransform: "uppercase", fontSize: 20,}}>This Month's top predictions</Text>
-                <View style={styles.MatchCard}>
-                    <Text style={styles.MatchCardTeams}>Man Utd vs Everton</Text>
-                    <Text style={styles.MatchCardPrediction}>Prediction: GG</Text>
-                    <Text style={styles.MatchCardStartTime}>Start: 1700hrs</Text>
-                </View>
-                <View style={styles.MatchCard}>
-                    <Text style={styles.MatchCardTeams}>man Utd vs Everton</Text>
-                    <Text style={styles.MatchCardPrediction}>Prediction: GG</Text>
-                    <Text style={styles.MatchCardStartTime}>Start: 1700hrs</Text>
-                </View>
-                <View style={styles.MatchCard}>
-                    <Text style={styles.MatchCardTeams}>Man Utd vs Everton</Text>
-                    <Text style={styles.MatchCardPrediction}>Prediction: GG</Text>
-                    <Text style={styles.MatchCardStartTime}>Start: 1700hrs</Text>
-                </View>
-                <Text style={styles.TotalOdds}>total odds: 20.86</Text>
-            </ScrollView>
-        </SafeAreaView>
+       <View
+        style={{ justifyContent: "center", alignItems: "center", height: 400 }}
+      >
+        <Text>I am the Tweets screen 🐐🐐🐐🐐🐐🐐</Text>
+      </View>
     );
+  }
 }
-const styles = StyleSheet.create({
-    container: {
-        paddingVertical: 10
-    },
-    buttonView: {
-        marginHorizontal: 50,
-        marginVertical: 10,
-        textTransform: "capitalize"
-    },
-    MatchCard: {
-        marginVertical: 10,
-        marginHorizontal: 50,
-        borderWidth: 0.5,
-        borderStyle: "solid",
-        borderColor: "#a9a9a9",
-        borderRadius: 5,
-        padding: 10,
-        borderBottomWidth: 2,
-        borderRightWidth: 2,
-    },
-    MatchCardTeams: {
-        fontSize: 20,
-        fontWeight: "600",
-        textTransform: "capitalize"
-    },
-    MatchCardPrediction: {
-        fontSize: 18,
-        fontWeight: "100"
-    },
-    MatchCardStartTime: {
-        fontSize: 18,
-        fontWeight: "100"
-    },
-    TotalOdds: {
-        fontSize: 20,
-        fontWeight: "bold",
-        marginHorizontal: 30,
-        textTransform: "uppercase"
-    }
-})
+
+export default Monthly;
